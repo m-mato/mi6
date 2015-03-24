0b806a263362065a8baaa3262b1c9bac693fbe5d
m-mato <mato.majdis@gmail.com>
2015-03-24 22:45:49 +0100

    Implemented AgentManagerImpl

--- mi6/src/managers/AgentManagerImpl.java
+++ mi6/src/managers/AgentManagerImpl.java
@@ -6,38 +6,173 @@
 package managers;
 
 import entities.Agent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Andrej Halaj
  * @author Matej Majdis
  */
-public class AgentManagerImpl implements AgentManager{
+public class AgentManagerImpl implements AgentManager {
+
+    private static final String DB_URL = "jdbc:derby://localhost:1527/mi6secretDB";
 
     @Override
     public void createAgent(Agent agent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (agent == null) {
+            String message = "Error in createAgent: agent cannot be null.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message);
+        }
+        
+        if (agent.getNickName() == null) {
+            String message = "Error in createAgent: agent name cannot be null.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message);
+        }
+
+        if (agent.getNickName().isEmpty()) {
+            String message = "Error in createAgent: agent name cannot be empty.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message);
+        }
+
+        if (agent.getAge() <= 0) {
+            String message = "Error in createAgent: agent name cannot be null.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message); 
+        }
+        
+        try (Connection conn = DriverManager.getConnection(DB_URL, "M", "12345")) {
+            PreparedStatement insertStatement = conn.prepareStatement("INSERT INTO M.AGENT (AGE, NICKNAME, PHONENUMBER) VALUES (?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+
+            insertStatement.setInt(1, agent.getAge());
+            insertStatement.setString(2, agent.getNickName());
+            insertStatement.setString(3, agent.getPhoneNumber());
+
+            insertStatement.executeUpdate();
+
+            ResultSet rs = insertStatement.getGeneratedKeys();
+            rs.next();
+            agent.setId(rs.getLong(1));
+        } catch (SQLException ex) {
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void updateAgent(Agent agent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (agent == null) {
+            String message = "Error in updateAgent: agent cannot be null.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message);
+        }
+        
+        if (agent.getNickName() == null) {
+            String message = "Error in updateAgent: agent name cannot be null.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message);
+        }
+
+        if (agent.getNickName().isEmpty()) {
+            String message = "Error in updateAgent: agent name cannot be empty.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message);
+        }
+
+        if (agent.getAge() <= 0) {
+            String message = "Error in updateAgent: agent name cannot be null.";
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.WARNING, message);
+            throw new IllegalArgumentException(message); 
+        }
+        
+        try (Connection conn = DriverManager.getConnection(DB_URL, "M", "12345")) {
+            PreparedStatement insertStatement = conn.prepareStatement("UPDATE M.AGENT SET AGE=?, NICKNAME=?, PHONENUMBER=? WHERE ID=?");
+
+            insertStatement.setInt(1, agent.getAge());
+            insertStatement.setString(2, agent.getNickName());
+            insertStatement.setString(3, agent.getPhoneNumber());
+            insertStatement.setLong(4, agent.getId());
+
+            insertStatement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void deleteAgent(Agent agent) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try (Connection conn = DriverManager.getConnection(DB_URL, "M", "12345")) {
+            PreparedStatement deleteStatement = conn.prepareStatement("DELETE FROM M.AGENT WHERE ID=?");
+
+            deleteStatement.setLong(1, agent.getId());
+
+            deleteStatement.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public Agent getAgentById(Long id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Agent agent;
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, "M", "12345")) {
+            PreparedStatement getStatement = conn.prepareStatement("SELECT AGE, NICKNAME, PHONENUMBER FROM M.AGENT WHERE ID=?");
+
+            getStatement.setLong(1, id);
+
+            ResultSet rs = getStatement.executeQuery();
+            if (rs.next()) {
+                agent = new Agent();
+                agent.setId(id);
+                agent.setAge(rs.getInt("AGE"));
+                agent.setNickName(rs.getString("NICKNAME"));
+                agent.setPhoneNumber(rs.getString("PHONENUMBER"));
+
+                return agent;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        return null;
     }
 
     @Override
     public List<Agent> findAllAgents() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       Agent agent;
+       List<Agent> agents = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(DB_URL, "M", "12345")) {
+            PreparedStatement findStatement = conn.prepareStatement("SELECT ID, AGE, NICKNAME, PHONENUMBER FROM M.AGENT");
+
+
+            ResultSet rs = findStatement.executeQuery();
+            
+            while (rs.next()) {
+                agent = new Agent();
+                agent.setId(rs.getLong(1));
+                agent.setAge(rs.getInt(2));
+                agent.setNickName(rs.getString(3));
+                agent.setPhoneNumber(rs.getString(4));
+
+                agents.add(agent);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return agents;
     }
-    
+
 }
--- mi6/test/managers/AgentManagerImplTest.java
+++ mi6/test/managers/AgentManagerImplTest.java
@@ -6,10 +6,16 @@
 package managers;
 
 import entities.Agent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -19,92 +25,94 @@ import static org.junit.Assert.*;
  * @author Andrej Halaj
  */
 public class AgentManagerImplTest {
-    
+
     private AgentManagerImpl agentManager;
-    
+
     @Before
     public void setUp() {
         agentManager = new AgentManagerImpl();
+        
+        deleteAllAgentsFromDatabase();
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testCreateNullAgent() {
         agentManager.createAgent(null);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testCreateAgentWithWrongAge() {
         Agent agent = newAgent(-3, "Devil", "555245845");
         agentManager.createAgent(agent);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testCreateAgentWithNullNickName() {
         Agent agent = newAgent(32, null, "555245845");
         agentManager.createAgent(agent);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testCreateAgentWithEmptyNickName() {
-        Agent agent = newAgent(32,"", "555213125");
+        Agent agent = newAgent(32, "", "555213125");
         agentManager.createAgent(agent);
     }
-    
+
     @Test
     public void testCreateAgentWithRightArguments() {
         Agent agent = newAgent(24, "Shadow", "555324846");
         agentManager.createAgent(agent);
         Long agentId = agent.getId();
-        
+
         Agent agentFromDatabase = agentManager.getAgentById(agentId);
         assertEquals(agent, agentFromDatabase);
         assertNotSame(agent, agentFromDatabase);
         assertDeepEquals(agent, agentFromDatabase);
-        
+
         agent = newAgent(32, "Devil", null); // doesn't need to have a phone number
         agentManager.createAgent(agent);
         Agent result = agentManager.getAgentById(agent.getId());
         assertNotNull(result);
-        
+
         agent = newAgent(21, "Devil", "555215485");
         agentManager.createAgent(agent);
         result = agentManager.getAgentById(agent.getId());
         assertNotNull(result);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testUpdateAgentWithWrongAge() {
         Agent agent = newAgent(24, "Shadow", "555324846");
         agentManager.createAgent(agent);
         Long agentId = agent.getId();
-        
+
         agent = agentManager.getAgentById(agentId);
         agent.setAge(0);
         agentManager.updateAgent(agent);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testUpdateAgentWithNullNickName() {
         Agent agent = newAgent(24, "Shadow", "555324846");
         agentManager.createAgent(agent);
         Long agentId = agent.getId();
-        
+
         agent = agentManager.getAgentById(agentId);
         agent.setNickName(null);
         agentManager.updateAgent(agent);
     }
-    
+
     @Test(expected = IllegalArgumentException.class)
     public void testUpdateAgentWithEmptyNickName() {
         Agent agent = newAgent(24, "Shadow", "555324846");
         agentManager.createAgent(agent);
         Long agentId = agent.getId();
-        
+
         agent = agentManager.getAgentById(agentId);
         agent.setNickName("");
         agentManager.updateAgent(agent);
     }
-    
+
     @Test
     public void testUpdateAgentWithRightArguments() {
         Agent agent = newAgent(24, "Shadow", "555324846");
@@ -117,21 +125,21 @@ public class AgentManagerImplTest {
         assertEquals(42, agent.getAge());
         assertEquals("Shadow", agent.getNickName());
         assertEquals("555324846", agent.getPhoneNumber());
-        
+
         agent = agentManager.getAgentById(agentId);
         agent.setNickName("Blue");
         agentManager.updateAgent(agent);
         assertEquals(42, agent.getAge());
         assertEquals("Blue", agent.getNickName());
         assertEquals("555324846", agent.getPhoneNumber());
-        
+
         agent = agentManager.getAgentById(agentId);
         agent.setPhoneNumber("555846217");
         agentManager.updateAgent(agent);
         assertEquals(42, agent.getAge());
         assertEquals("Blue", agent.getNickName());
         assertEquals("555846217", agent.getPhoneNumber());
-        
+
         agent = agentManager.getAgentById(agentId);
         agent.setPhoneNumber(null);
         agentManager.updateAgent(agent);
@@ -139,82 +147,80 @@ public class AgentManagerImplTest {
         assertEquals("Blue", agent.getNickName());
         assertNull(agent.getPhoneNumber());
     }
-    
+
     @Test
     public void testDeleteAgent() {
         Agent agent = newAgent(24, "Shadow", "555324846");
         Agent agent2 = newAgent(31, "Devil", "555241325");
         agentManager.createAgent(agent);
         agentManager.createAgent(agent2);
-        
+
         List<Agent> expected = Arrays.asList(agent, agent2);
         List<Agent> actual = agentManager.findAllAgents();
         assertEquals(actual, expected);
         assertDeepEquals(actual, expected);
-        
+
         Long agentId = agent.getId();
         Long agent2Id = agent2.getId();
-        
+
         agent = agentManager.getAgentById(agentId);
         agent2 = agentManager.getAgentById(agent2Id);
-        
+
         agentManager.deleteAgent(agent);
-        
+
         expected = Arrays.asList(agent2);
         actual = agentManager.findAllAgents();
         assertEquals(actual, expected);
         assertDeepEquals(actual, expected);
     }
-    
+
     @Test
     public void testGetAgentById() {
         Agent agent1 = newAgent(24, "Shadow", "555324846");
         Agent agent2 = newAgent(32, "Devil", "555245845");
-        
+
         agentManager.createAgent(agent1);
         agentManager.createAgent(agent2);
-        
+
         Agent agentFromDatabase1 = agentManager.getAgentById(agent1.getId());
         Agent agentFromDatabase2 = agentManager.getAgentById(agent2.getId());
-        
+
         assertEquals(agentFromDatabase1, agent1);
         assertEquals(agentFromDatabase2, agent2);
         assertDeepEquals(agentFromDatabase1, agent1);
         assertDeepEquals(agentFromDatabase2, agent2);
     }
-    
+
     @Test
     public void testFindAllAgents() {
-        assertTrue(agentManager.findAllAgents().isEmpty());
-        
+
         Agent agent1 = newAgent(24, "Shadow", "555324846");
         Agent agent2 = newAgent(32, "Devil", "555245845");
         Agent agent3 = newAgent(28, "Ghost", "555425451");
-        
+
         agentManager.createAgent(agent1);
         agentManager.createAgent(agent2);
         agentManager.createAgent(agent3);
-        
+
         List<Agent> expected = Arrays.asList(agent1, agent2, agent3);
         List<Agent> actual = agentManager.findAllAgents();
-        
+
         Collections.sort(expected, agentIdComparator);
         Collections.sort(actual, agentIdComparator);
-        
+
         assertEquals(expected, actual);
         assertDeepEquals(expected, actual);
     }
-    
-    public Agent newAgent(int age, String nickName, String phoneNumber)
-    {
+
+    public Agent newAgent(int age, String nickName, String phoneNumber) {
         Agent newAgent = new Agent();
         newAgent.setAge(age);
         newAgent.setNickName(nickName);
         newAgent.setPhoneNumber(phoneNumber);
-        
+
         return newAgent;
     }
-    
+
     private void assertDeepEquals(List<Agent> expectedList, List<Agent> actualList) {
         for (int i = 0; i < expectedList.size(); i++) {
             Agent expected = expectedList.get(i);
@@ -229,13 +235,25 @@ public class AgentManagerImplTest {
         assertEquals(expected.getNickName(), actual.getNickName());
         assertEquals(expected.getPhoneNumber(), actual.getPhoneNumber());
     }
-    
+
     private static Comparator<Agent> agentIdComparator = new Comparator<Agent>() {
 
         @Override
         public int compare(Agent o1, Agent o2) {
-            return Long.valueOf(o1.getId()).compareTo(Long.valueOf(o2.getId()));
+            return o1.getId().compareTo(o2.getId());
         }
     };
-    
+
+    private void deleteAllAgentsFromDatabase() {
+        final String DB_URL = "jdbc:derby://localhost:1527/mi6secretDB";
+        
+        try (Connection conn = DriverManager.getConnection(DB_URL, "M", "12345")) {
+            PreparedStatement deleteStatement = conn.prepareStatement("TRUNCATE TABLE M.AGENT");
+
+            deleteStatement.execute();
+
+        } catch (SQLException ex) {
+            Logger.getLogger(AgentManagerImpl.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
