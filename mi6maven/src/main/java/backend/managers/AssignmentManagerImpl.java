b7284dabe1f920c78c19ca01e3d8e34f5971b94d
m-mato <mato.majdis@gmail.com>
2015-04-21 20:22:28 +0200

    SpringConfig.java

--- mi6maven/src/main/java/backend/managers/AssignmentManagerImpl.java
+++ mi6maven/src/main/java/backend/managers/AssignmentManagerImpl.java
@@ -14,7 +14,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.List;
 import java.util.logging.Level;
